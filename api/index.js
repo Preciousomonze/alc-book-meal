11698e0479ea22a2c4c4a7832ec0dab6c448ccad
Omonzejele Precious <crispusomonze@gmail.com>
2019-02-10 01:24:04 +0100

    added more structure

---
+++ api/index.js
@@ -0,0 +1,10 @@
+
+import express from './express';
+import mealsRoute from './routes/mealsRoute';
+
+const app = express();
+
+app.use('/api/v1/meals', mealsRoute);
+
+const PORT = 7000;
+app.listen(PORT);
