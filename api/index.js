e696801f8744c6afb9734393767dc81d27009b8e
Omonzejele Precious <crispusomonze@gmail.com>
2019-02-15 22:21:15 +0100

    installed necessary dependencies for now

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-
 import express from 'express';
-import mealsRoute from './routes/mealsRoute';
+import Routes from '/routes/routes';
+// thanks to @timsprof for the inspiration
 
 const app = express();
 
-app.use('/api/v1/meals', mealsRoute);
+app.use('/api/v1/', Routes);
 
 const PORT = 7000;
 app.listen(PORT);
