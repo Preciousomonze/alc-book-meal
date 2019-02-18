02d708a73fbd247c7dc56709a1d441f8a7e25640
Omonzejele Precious <crispusomonze@gmail.com>
2019-02-18 11:29:57 +0100

    finally done with all controllers :)

---
+++ api/controllers/meal.controller.js
@@ -0,0 +1,54 @@
+import MealService from '../services/meal.service';
+
+const MealController = {
+  getMeals(req, res) {
+    const allMeals = MealService.getMeals();
+    return res
+      .json({
+        status: 'success',
+        data: allMeals,
+      })
+      .status(200);
+  },
+  addMeal(req, res) {
+    /*
+            Expect json of the format
+            {
+                name: "some food",
+                size: "Large",
+                "price": 400
+            }
+        */
+    const newMeal = req.body;
+    const createdMeal = MealService.addMeal(newMeal);
+    return res
+      .json({
+        status: 'success',
+        data: createdMeal,
+      })
+      .status(201);
+  },
+  getMeal(req, res) {
+    // eslint-disable-next-line prefer-destructuring
+    const id = req.params.id;
+    const getMeal = MealService.getMeal(id);
+    return res
+      .json({
+        status: 'success',
+        data: getMeal,
+      })
+      .status(200);
+  },
+  deleteMeal(req, res) {
+    const { id } = req.params;
+    const meals = MealService.deleteMeal(id);
+    return res
+      .json({
+        status: 'success',
+        data: meals,
+      })
+      .status(200);
+  },
+};
+
+export default MealController;
---
+++ api/controllers/order.controller.js
@@ -0,0 +1,42 @@
+import OrderServices from '../services/order.service';
+
+const OrderController = {
+  addOrder(req, res) {
+    const orderAdded = req.body;
+    const addOrder = OrderServices.addOrder(orderAdded);
+    return res
+      .json({
+        status: 'success',
+        data: addOrder,
+      })
+      .status(200);
+  },
+  getOrders(req, res) {
+    const allOrders = OrderServices.getOrders();
+    return res
+      .json({
+        status: 'success',
+        allOrders,
+      })
+      .status(200);
+  },
+  updateOrder(req, res) {
+    const updatedOrder = req.body;
+    const { id } = req.params;
+    const orderStuff = OrderServices.updateOrder(id, updatedOrder);
+    return res.json({
+      status: 'success',
+      orderStuff,
+    });
+  },
+  deleteAnOrder(req, res) {
+    const { id } = req.params;
+    const deletedStatus = OrderServices.deleteAnOrder(id);
+    return res.json({
+      deletedStatus,
+    })
+      .status(200);
+  },
+};
+
+export default OrderController;
