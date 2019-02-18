f862052c97adb2c36dfb9defab3da5d5ed7642ff
Omonzejele Precious <crispusomonze@gmail.com>
2019-02-18 11:45:49 +0100

    fixing route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import Routes from '/routes/routes';
+import orderRouter from './routes/order.route';
+import mealRouter from './routes/meal.route';
+import menuRouter from './routes/menu.route';
 // thanks to @timsprof for the inspiration
 
 const app = express();
+const apiNamespace = '/api/v1/';
 
-app.use('/api/v1/', Routes);
+app.use(apiNamespace, orderRouter);
+app.use(apiNamespace, mealRouter);
+app.use(apiNamespace, menuRouter);
 
 const PORT = 7000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`yae, server running on port ${PORT} :)`);
+});
---
+++ api/routes/menu.route.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+// controller
+import MenuController from '../controllers/menu.controller';
+
+const router = Router();
+
+// add menu controller
+router.get('menu/', MenuController.getMenu);
+
+router.post('menu/', MenuController.addMenu);
+
+const menuRouter = router;
+
+export default menuRouter;
