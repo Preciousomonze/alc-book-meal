9e74a2d7de13e752547878ba59776b6aedc3afa3
Omonzejele Precious <crispusomonze@gmail.com>
2019-02-18 10:11:38 +0100

    structured dummy data

---
+++ api/data/menuDummyData.js
@@ -0,0 +1,13 @@
+export default {
+  menu: [
+    {
+      date: '18-02-2019',
+      meal: [
+        {
+          name: 'Chicken Salad',
+          size: 'Medium',
+        },
+      ],
+    },
+  ],
+};
