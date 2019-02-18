36c3831e1704e1831e7e6f32c6c2ce8e76a2a016
Omonzejele Precious <crispusomonze@gmail.com>
2019-02-18 09:05:20 +0100

    pushing meals

--- api/models/Meals.js
+++
@@ -1,9 +0,0 @@
-export default class Meals {
-    constructor() {
-      this.id = null;
-      this.name = null;
-      this.size = null;
-      this.price = null;
-      this.currency = null;
-    }
-  }
