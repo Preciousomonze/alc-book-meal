7f073e17f309c354d27111ab1a77bb5f6b48bdec
Omonzejele Precious <crispusomonze@gmail.com>
2019-02-25 16:26:29 +0100

    fixed order route bug

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -4,11 +4,11 @@ import OrderController from '../controllers/order.controller';
 
 const orderRouter = Router();
 
-orderRouter.get('/order/', OrderController.getOrders);
+orderRouter.get('/orders/', OrderController.getOrders);
 
 orderRouter.post('/order/', OrderController.addOrder);
 
 orderRouter.put('/order/:id', OrderController.updateOrder);
 
-orderRouter.delete('order/:id', OrderController.deleteOrder);
+orderRouter.delete('/order/:id', OrderController.deleteOrder);
 export default orderRouter;
